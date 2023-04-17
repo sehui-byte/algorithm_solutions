7ce37de15006e6dbf487e6e4a64a43291ce77fc0
sehui-byte <kimsell1859@gmail.com>
2023-04-17 21:31:48 +0900

    백준 9935번 solved : stack 사용

--- BOJ/boj/BOJ9935.java
+++ BOJ/boj/BOJ9935.java
@@ -1,27 +1,46 @@
 package boj;
 
 import java.util.Scanner;
+import java.util.Stack;
 
-// 폭발 문자열
+/**
+ * 1. replaceAll() -> 메모리초과
+ * 2. stack을 활용하여 다시 품.
+ */
 public class BOJ9935 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         String s = sc.nextLine();
-        String blast = sc.nextLine();
+        String bomb = sc.nextLine();
 
-        while(true) {
-            if(s.length() == 0) {
-                System.out.println("FRULA");
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            stack.push(s.charAt(i));
+
+            if (stack.size() >= bomb.length()) {
+                boolean isSame = true;
+                for (int j = 0; j < bomb.length(); j++) {
+                    if (stack.get(stack.size() - bomb.length() + j) != bomb.charAt(j)) {
+                        isSame = false;
+                        break;
+                    }
+                }
+
+                if (isSame) {
+                    for (int k = 0; k < bomb.length(); k++) {
+                        stack.pop();
+                    }
+                }
             }
-
-            if(s.indexOf(blast) == -1) {
-                System.out.print(s);
-                break;
-            }
-
-            s = s.replaceAll(blast, "");
         }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : stack) {
+            sb.append(c);
+        }
+
+        System.out.println(sb.length() > 0 ? sb.toString() : "FRULA");
     }
 }
