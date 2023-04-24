deccc22e00b3b6fb6c21781094f22963257965d5
sehui-byte <kimsell1859@gmail.com>
2023-04-24 17:51:42 +0900

    백준 3040번 백설공주와 일곱난쟁이 - 리팩토링

--- BOJ/boj/basic/BOJ3040.java
+++ BOJ/boj/basic/BOJ3040.java
@@ -3,35 +3,42 @@ package boj.basic;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * @문제명 백설공주와 일곱난쟁이
+ * @분류 브루트포스
+ * @풀이 9명의 난쟁이 중에서 가짜 난쟁이 2명을 골라야 함.
+ *      2중 for문 돌려서 난쟁이 둘을 제거했을 때 합이 100이 나오는지 체크
+ */
 public class BOJ3040 {
-    static int[] arr = new int[9];
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] arr = new int[9];
 
         for (int i = 0; i < 9; i++) {
             arr[i] = sc.nextInt();
         }
 
-        int a = 0; int b = 0;
+        int a = 0;
+        int b = 0;
+
+        int total = Arrays.stream(arr).sum();
 
         for (int i = 0; i < 9; i++) {
-            int sum = Arrays.stream(arr).sum();
-            sum -= arr[i];
+            int sum = total;
             for (int j = i + 1; j < 9; j++) {
-                sum -= arr[j];
+                sum = total - (arr[i] + arr[j]);
                 if (sum == 100) {
-                    b = j; a = i; print(a,b);
-                    return;
+                    a = i;
+                    b = j;
+                    break;
                 }
-                sum += arr[j];
             }
         }
-    }
 
-    private static void print(int a, int b) {
         for (int i = 0; i < 9; i++) {
-            if (i == a || i == b) continue;
+            if (i == a || i == b) {
+                continue;
+            }
             System.out.println(arr[i]);
         }
     }
