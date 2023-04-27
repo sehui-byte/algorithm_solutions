38214889530bdfac0b741af362450c5bc395a25c
sehui-byte <kimsell1859@gmail.com>
2023-04-27 19:33:00 +0900

    백준 18511 큰 수 구성하기 solved

--- BOJ/boj/basic/BOJ18511.java
+++ BOJ/boj/basic/BOJ18511.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * @문제 N보다 작거나 같은 자연수 중에서 집합 K의 원소로만 구성된 가장 큰 수를 출력
  * K의 원소의 개수 ≤ 3
  * N=657, K={1,5,7} => 답: 577
- * @풀이
+ * @분류 완전탐색, 재귀
  */
 public class BOJ18511 {
     public static void main(String[] args) {
@@ -23,23 +23,18 @@ public class BOJ18511 {
 
         Arrays.sort(k);
 
-        int result = find_max(String.valueOf(n).length(), 0, n, k);
+        int result = find_max(n, k, 0);
         System.out.println(result);
 
     }
 
-    private static int find_max(int digit, int currentMax, int n, int[] arr) {
-        if (digit == 0 && currentMax <= n) return currentMax;
-
-        int max = 0;
-        for (int i = arr.length - 1; i >= 0; i--) {
-            int num = currentMax * 10 + arr[i];
-            if (num <= n) {
-                int tmp = find_max(digit - 1, num, n, arr);
-                max = Math.max(tmp, max);
-            }
+    private static int find_max(int n, int[] k, int currentMax) {
+        if(currentMax > n) return 0;
+        int max = currentMax;
+        for (int i = k.length - 1; i >= 0; i--) {
+            int tmp = currentMax * 10 + k[i];
+            max = Math.max(max, find_max(n, k, tmp));
         }
-
         return max;
     }
 }
