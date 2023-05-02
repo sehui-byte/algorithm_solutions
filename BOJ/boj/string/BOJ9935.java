6fc3dd66123a1b17415be2ade78a277915c96738
sehui-byte <kimsell1859@gmail.com>
2023-05-02 11:35:24 +0900

    백준 9935 문자열 폭발 풀이설명 추가

--- BOJ/boj/string/BOJ9935.java
+++ BOJ/boj/string/BOJ9935.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 import java.util.Stack;
 
 /**
- * 1. replaceAll() -> 메모리초과
- * 2. stack을 활용하여 다시 풂.
+ * 1. replaceAll() -> 메모리초과 (Pattern객체가 계속 새로 생성되기 때문)
+ * 2. stack을 활용하여 다시 풂 :
+ *          스택에 문자열을 넣고 폭발 문자열 길이만큼 꺼내서 비교한다.
+ *          폭발 문자열과 일치하면 스택에서 문자열을 꺼낸다.
+ *          최종적으로 스택에 남은 문자열을 꺼내서 이어붙이면 결과값이 된다.
  */
 public class BOJ9935 {
 
