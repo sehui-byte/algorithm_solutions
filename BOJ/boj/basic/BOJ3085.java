bb120e4e83144d44906a7ff39765674c319eab0f
sehui-byte <kimsell1859@gmail.com>
2023-05-04 15:55:04 +0900

    refactoring : 백준3085 사탕게임

--- BOJ/boj/basic/BOJ3085.java
+++ BOJ/boj/basic/BOJ3085.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 /**
  * @문제명 사탕 게임
  * @분류 완전탐색
+ * @풀이 상하좌우를 전체 다 swap할 필요 없이 하단과 오른쪽만 swap해주면 된다.
+ * 결국 arr[i][j-1]을 오른쪽 arr[i][j]와 swap == arr[i][j]을 왼쪽 arr[i][j-1]과 swap
+ * 이기 때문이다.
  */
 public class BOJ3085 {
     static char[][] arr;
@@ -27,36 +30,39 @@ public class BOJ3085 {
     static private void findMax() {
         int max = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                //인접한 사탕 swap
-                max = Math.max(countMaxCandyCount(swap(i, j, i, j - 1)), max);
-                max = Math.max(countMaxCandyCount(swap(i, j, i, j + 1)), max);
-                max = Math.max(countMaxCandyCount(swap(i, j, i + 1, j)), max);
-                max = Math.max(countMaxCandyCount(swap(i, j, i - 1, j)), max);
+            for (int j = 0; j < n - 1; j++) {
+                // 인접한 사탕(오른쪽과 아래쪽) swap
+                swap(i, j, i, j + 1);
+                max = Math.max(findMaxCandyCount(), max);
+                swap(i, j + 1, i, j); //swap했던걸 돌려놓음
+            }
+        }
 
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n; j++) {
+                swap(i, j, i + 1, j);
+                max = Math.max(findMaxCandyCount(), max);
+                swap(i + 1, j, i, j);
             }
         }
         System.out.println(max);
     }
 
-    static int countMaxCandyCount(char[][] clone) {
-        if (clone == null) return -1;
+    static int findMaxCandyCount() {
         int max = 0;
-        // row 체크
         for (int i = 0; i < n; i++) {
+            // row 체크
             int count = 1;
             for (int j = 0; j < n - 1; j++) {
-                if (clone[i][j] == clone[i][j + 1]) count++;
+                if (arr[i][j] == arr[i][j + 1]) count++;
                 else count = 1;
                 max = Math.max(max, count);
             }
-        }
 
-        // column 체크
-        for (int i = 0; i < n; i++) {
-            int count = 1;
-            for (int j = 0; j < n-1; j++) {
-                if (clone[j][i] == clone[j + 1][i]) count++;
+            // column 체크
+            count = 1;
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j][i] == arr[j + 1][i]) count++;
                 else count = 1;
                 max = Math.max(max, count);
             }
@@ -64,20 +70,9 @@ public class BOJ3085 {
         return max;
     }
 
-    static private char[][] swap(int i, int j, int a, int b) {
-        if (a < 0 || a > n - 1 || b < 0 || b > n - 1) {
-            return null;
-        }
-        // deep copy
-        char[][] clone = new char[n][n];
-        for(int x = 0; x<n; x++) {
-            clone[x] = arr[x].clone();
-        }
-
-        char tmp = clone[i][j];
-        clone[i][j] = clone[a][b];
-        clone[a][b] = tmp;
-
-        return clone;
+    static private void swap(int i, int j, int a, int b) {
+        char tmp = arr[i][j];
+        arr[i][j] = arr[a][b];
+        arr[a][b] = tmp;
     }
 }
