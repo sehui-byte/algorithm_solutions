b8ffaa95c9dd21a2455aae38935b0cdf68738d1c
sehui-byte <kimsell1859@gmail.com>
2023-05-04 16:49:31 +0900

    백준 9935 복습 : 다시 풀어보기

--- BOJ/boj/string/BOJ9935.java
+++ BOJ/boj/string/BOJ9935.java
@@ -3,47 +3,42 @@ package boj.string;
 import java.util.Scanner;
 import java.util.Stack;
 
-/**
- * 1. replaceAll() -> 메모리초과 (Pattern객체가 계속 새로 생성되기 때문)
- * 2. stack을 활용하여 다시 풂 :
- *          스택에 문자열을 넣고 폭발 문자열 길이만큼 꺼내서 비교한다.
- *          폭발 문자열과 일치하면 스택에서 문자열을 꺼낸다.
- *          최종적으로 스택에 남은 문자열을 꺼내서 이어붙이면 결과값이 된다.
- */
 public class BOJ9935 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         String bomb = sc.nextLine();
 
-        Stack<Character> stack = new Stack<>();
-
+        Stack<Character> stack = new Stack<Character>();
         for (int i = 0; i < s.length(); i++) {
             stack.push(s.charAt(i));
 
+            // stack에서 폭발문자열 길이만큼 꺼내서 비교
             if (stack.size() >= bomb.length()) {
                 boolean isSame = true;
                 for (int j = 0; j < bomb.length(); j++) {
-                    if (stack.get(stack.size() - bomb.length() + j) != bomb.charAt(j)) {
+                    if (bomb.charAt(j) != stack.get(stack.size() - bomb.length() + j)) {
                         isSame = false;
-                        break;
+                        break; //시간초과 발생 안하도록 break문 넣어준다
                     }
                 }
 
                 if (isSame) {
-                    for (int k = 0; k < bomb.length(); k++) {
+                    for (int j = 0; j < bomb.length(); j++) {
                         stack.pop();
                     }
                 }
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (char c : stack) {
-            sb.append(c);
+        if (stack.isEmpty()) System.out.println("FRULA");
+        else {
+            StringBuilder sb = new StringBuilder();
+            for (Character c : stack) {
+                sb.append(c);
+            }
+            System.out.println(sb.toString());
         }
 
-        System.out.println(sb.length() > 0 ? sb.toString() : "FRULA");
     }
 }
